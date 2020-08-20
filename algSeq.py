bc5c1b5ed2ec41c8adefcb0b6ffc1ffebb56fed9
baton96 <podolce0@gmail.com>
2020-08-20 17:00:13 +0200

    Seq to obj-oriented refactor

--- algSeq.py
+++ algSeq.py
@@ -1,4 +1,13 @@
 import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Task:
+    p: int  # processing time
+    r: int  # ready time
+    d: int  # due time
+    i: int  # id
 
 
 def loadLine(file):
@@ -7,19 +16,15 @@ def loadLine(file):
 
 # I had to write my own argmin and argmax functions as we
 # weren't allowed to used any of non-built-in libs like numpy
-def argmin(arr, key=lambda x: x[1]):
-    return min(enumerate(arr), key=key)[0]
+def argmin(arr):
+    return min(enumerate(arr), key=lambda x: x[1])[0]
 
-
-def argmax(arr, key=lambda x: x[1]):
-    return max(enumerate(arr), key=key)[0]
-#
 
 def algList():
     with open(sys.argv[1], 'r') as file:
         n = loadLine(file)[0]
-        tasks = [loadLine(file) + [i] for i in range(n)]
-    tasks = sorted(tasks, key=lambda x: x[1])
+        tasks = [Task(i=i, *loadLine(file)) for i in range(n)]
+    tasks = sorted(tasks, key=lambda x: x.r)
 
     awaiting = []
     schedules = [[], [], [], []]
@@ -32,7 +37,7 @@ def algList():
         timerId = argmin(timers)
         if it != n:
             for i in range(it, n):
-                if tasks[i][1] <= timers[timerId]:
+                if tasks[i].r <= timers[timerId]:
                     awaiting += [tasks[i]]
                 else:
                     it = i
@@ -40,20 +45,20 @@ def algList():
             else:
                 it = n
         if not awaiting:
-            timers[timerId] = tasks[it][1]
+            timers[timerId] = tasks[it].r
             for i in range(it, n):
-                if tasks[i][1] <= timers[timerId]:
+                if tasks[i].r <= timers[timerId]:
                     awaiting += [tasks[i]]
                 else:
                     it = i
                     break
             else:
                 it = n
-        awaiting.sort(key=lambda x: (min(0, timers[timerId] + x[0] - x[2]), -x[0]))
+        awaiting.sort(key=lambda x: (min(0, timers[timerId] + x.p - x.d), -x.p))
         popped = awaiting.pop()
         schedules[timerId] += [popped]
-        timers[timerId] = max(timers[timerId], popped[1]) + popped[0]
-        criterium += max(0, timers[timerId] - popped[2])
+        timers[timerId] = max(timers[timerId], popped.r) + popped.p
+        criterium += max(0, timers[timerId] - popped.d)
         counter += 1
         if counter == n:
             break
@@ -63,7 +68,7 @@ def algList():
             str(criterium) + '\n' +
             '\n'.join([
                 ' '.join([
-                    str(task[3] + 1) for task in schedule
+                    str(task.i + 1) for task in schedule
                 ]) for schedule in schedules
             ])
         )
